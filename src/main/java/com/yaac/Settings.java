1710fce3449012a6469f76262aa01408cf97b143
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2023-06-14 07:13:59 +0200

    Init resolutions array before reading from savefile so as to prevent missing resolutions array if SaveFileManager creates a savefile

--- src/main/java/com/yaac/Settings.java
+++ src/main/java/com/yaac/Settings.java
@@ -11,11 +11,11 @@ public class Settings {
     public static final Logger LOGGER = Logger.getLogger(Settings.class.getName());
     public static final Level logLevel = Level.INFO;
     public static final String TITLE = "YAAC - Yet Another Asteroids Clone";
+    public static final String[] resolutions = new String[]{"1920x1080", "1280x720"};
     public static int width = Integer.parseInt(SaveFileManager.getInstance().getResolution().split("x")[0]);
     public static int height = Integer.parseInt(SaveFileManager.getInstance().getResolution().split("x")[1]);
     public static int superSecretCounter = 0;
     public static boolean superSecretValue = true;
-    public static final String[] resolutions = new String[]{"1920x1080", "1280x720"};
     public static final int shipSize = (int) (Game.getInstance().getSpaceShip().getRadius()*2);
     public static final String FONT = "Font.ttf";
     public static final float FONT_SIZE = 35f;
